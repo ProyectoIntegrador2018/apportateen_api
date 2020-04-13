a5e00542f5d77f8ee3bf1c2592bcd2081255c4f7
ValeriaRocha <valerocha_96@hotmail.com>
2020-04-12 19:38:35 -0500

    Detalle taller (#10)
    * First try fotos talleres
    
    * Changes to many images
    
    * llamada para obtener info de un taller
    
    Co-authored-by: Adrian <adrianpena_94@hotmail.com>

--- routes/talleres.js
+++ routes/talleres.js
@@ -4,6 +4,7 @@ var db = require('../queries');
 
 
 router.get('/', db.getTalleres);
+router.get('/detalle/:id', db.getTaller);
 router.get('/:id', db.getCorreosByTallerId);
 router.post('/', db.createTaller);
 router.put('/:id', db.updateTaller);
