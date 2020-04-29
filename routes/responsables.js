16146f518c1c7ee32f05ac5bd2d140721ec67836
Jonathan Cárdenas <JonathanCrd@users.noreply.github.com>
2020-04-29 01:48:37 -0500

    Modificar sede con responsable

--- routes/responsables.js
+++ routes/responsables.js
@@ -5,6 +5,7 @@ var db = require('../queries');
 
 router.get('/:correo_responsable', db.getIDResponsable);
 router.post('/', db.createResponsable);
+router.put('/:id', db.updateResponsable);
 
 
 module.exports = router;
