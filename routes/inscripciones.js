31822a613d3e0a2f9693b773267ffde224a9311e
Valeria Rocha <valerocha_96@hotmail.com>
2020-05-21 11:49:41 -0500

    Pagar Taller (#34)
    * subir comprobante de pago
    
    * small fix

--- routes/inscripciones.js
+++ routes/inscripciones.js
@@ -6,6 +6,6 @@ var db = require('../queries');
 router.post('/', db.createInscripcion);
 router.get('/:user_id', db.getTalleresInscritos);
 router.delete('/:taller_id/:user_id', db.removeInscripcion);
-
+router.put('/comprobante', db.subirComprobante);
 
 module.exports = router;
